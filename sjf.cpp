6a8f46dc087b93115f58d4b9f19d352c166dd8ea
code-righter <abhijeetkolhe38@gmail.com>
2025-12-02 07:44:22 +0530

    new

--- sjf.cpp
+++ sjf.cpp
@@ -1,112 +1,106 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
-
+#include <climits>
+#include <iomanip>
 using namespace std;
 
 struct Process {
-    int id;
-    int AT;
-    int BT;
-    int FT;
-    int TAT;
-    int WT;
+    int pid;
+    int arrival;
+    int burst;
+    int remaining;
+    int finish;
+    int tat;
+    int wait;
+    int response;
+    bool finished;
+    bool started;
 };
 
+// --------------------------
+// NON-PREEMPTIVE SJF
+// --------------------------
+void sjf_non_preemptive(vector<Process> p) {
+    int n = p.size();
+    int current_time = 0, completed = 0;
+    double total_tat = 0, total_wait = 0, total_rt = 0;
+
+    // sort by arrival time
+    sort(p.begin(), p.end(), [](const Process &a, const Process &b) {
+        return a.arrival < b.arrival;
+    });
+
+    while (completed < n) {
+        int idx = -1, min_bt = INT_MAX;
+
+        // find process with minimum burst time
+        for (int i = 0; i < n; i++) {
+            if (!p[i].finished && p[i].arrival <= current_time) {
+                if (p[i].burst < min_bt) {
+                    min_bt = p[i].burst;
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1) {
+            current_time++;
+            continue;
+        }
+
+        // set response time (only first time)
+        p[idx].response = current_time - p[idx].arrival;
+
+        // run fully
+        current_time += p[idx].burst;
+        p[idx].finish = current_time;
+        p[idx].tat = p[idx].finish - p[idx].arrival;
+        p[idx].wait = p[idx].tat - p[idx].burst;
+        p[idx].finished = true;
+
+        total_tat += p[idx].tat;
+        total_wait += p[idx].wait;
+        total_rt += p[idx].response;
+        completed++;
+    }
+
+    cout << "\nPID\tAT\tBT\tFT\tTAT\tWT\tRT\n";
+    for (int i = 0; i < n; i++) {
+        cout << p[i].pid << "\t" << p[i].arrival << "\t" << p[i].burst << "\t"
+             << p[i].finish << "\t" << p[i].tat << "\t" << p[i].wait << "\t"
+             << p[i].response << "\n";
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "\nAverage Turnaround Time = " << total_tat / n;
+    cout << "\nAverage Waiting Time    = " << total_wait / n;
+    cout << "\nAverage Response Time   = " << total_rt / n << endl;
+}
+
+
+// --------------------------
+// MAIN
+// --------------------------
 int main() {
-    int n;
+    int n, choice;
+
     cout << "Enter number of processes: ";
     cin >> n;
 
     vector<Process> p(n);
-    vector<int> rem(n);
 
     for (int i = 0; i < n; i++) {
-        p[i].id = i + 1;
-        cout << "Arrival Time of P" << i+1 << ": ";
-        cin >> p[i].AT;
-        cout << "Burst Time of P" << i+1 << ": ";
-        cin >> p[i].BT;
-        rem[i] = p[i].BT;
+        p[i].pid = i + 1;
+        cout << "Arrival time of P" << p[i].pid << ": ";
+        cin >> p[i].arrival;
+        cout << "Burst time of P" << p[i].pid << ": ";
+        cin >> p[i].burst;
     }
 
-    int tq;
-    cout << "Enter Time Quantum: ";
-    cin >> tq;
-
-    // sort by arrival time
-    sort(p.begin(), p.end(), [](Process &a, Process &b){
-        return a.AT < b.AT;
-    });
-
-    queue<int> q;
-    int t = 0, completed = 0, ptr = 0;
-
-    if (ptr < n && t < p[ptr].AT)
-        t = p[ptr].AT;
-
-    while (ptr < n && p[ptr].AT <= t) {
-        q.push(ptr);
-        ptr++;
-    }
-
-    while (completed < n) {
-
-        if (q.empty()) {
-            if (ptr < n) {
-                t = p[ptr].AT;
-                while (ptr < n && p[ptr].AT <= t) {
-                    q.push(ptr);
-                    ptr++;
-                }
-            }
-            continue;
-        }
-
-        int i = q.front();
-        q.pop();
-
-        int use = (rem[i] < tq) ? rem[i] : tq;
-        rem[i] -= use;
-        t += use;
-
-        while (ptr < n && p[ptr].AT <= t) {
-            q.push(ptr);
-            ptr++;
-        }
-
-        if (rem[i] == 0) {
-            p[i].FT = t;
-            completed++;
-        } else {
-            q.push(i);
-        }
-    }
-
-    // compute TAT and WT + averages
-    double totalTAT = 0, totalWT = 0;
-
-    for (int i = 0; i < n; i++) {
-        p[i].TAT = p[i].FT - p[i].AT;
-        p[i].WT  = p[i].TAT - p[i].BT;
-
-        totalTAT += p[i].TAT;
-        totalWT  += p[i].WT;
-    }
-
-    cout << "\nProcess\tAT\tBT\tFT\tTAT\tWT\n";
-    for (int i = 0; i < n; i++) {
-        cout << "P" << p[i].id << "\t"
-             << p[i].AT << "\t"
-             << p[i].BT << "\t"
-             << p[i].FT << "\t"
-             << p[i].TAT << "\t"
-             << p[i].WT << "\n";
-    }
-
-    cout << "\nAverage TAT = " << totalTAT / n;
-    cout << "\nAverage WT  = " << totalWT / n << "\n";
+    
+    sjf_non_preemptive(p);
+    
 
     return 0;
 }
